4d6d8a18a5f07a8826fbc66e3791ad3b4cc219d3
Your Name <Lancelot@Lancelot.local>
2014-11-29 16:48:03 +0900

    removed bugs

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,25 +1,51 @@
 #include "ofApp.h"
-#include "CircleState.h"
-#include "BoxState.h"
-#include "ParticleState.h"
-
-float *magnitudeL;
-float *magnitudeR;
-int fft_size;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     
-    //    stateMachine.getSharedData().sharedMsg = "Here's shared message!";
+    ofSetVerticalSync(true);
+    ofEnableDepthTest();
     
-    // StateMachineを初期化
-    stateMachine.addState<CircleState>();
-    stateMachine.addState<BoxState>();
-    stateMachine.addState<ParticleState>();
+    ofBackground(0, 0, 0);
     
-    stateMachine.changeState("circle");
+    myFbo_00.allocate(1024, 768, GL_RGBA);
+    myFbo_01.allocate(1024, 768, GL_RGBA);
+    myGlitch_00.setup(&myFbo_00);
+    myGlitch_01.setup(&myFbo_01);
+    
+    myFbo_00.begin();
+    ofClear(255, 255, 255, 0);
+    myFbo_00.end();
+    
+    myFbo_01.begin();
+    ofClear(255, 255, 255, 0);
+    myFbo_01.end();
     
     
+    // nodes
+    testNodes[0].setOrientation(ofVec3f(30,0,40));
+    testNodes[1].setOrientation(ofVec3f(40,0,30));
+    
+    
+    // cameras
+    camToView = 0;
+    cam[0].setPosition(40, 40, 500);
+    cam[1].setPosition(40, 300, 100);
+    lookatIndex[1] = kNumTestNodes-1;
+    
+    
+    // lights
+    light.enable();
+    light.setSpotlight();
+    light.setPosition(50, 80, 150);
+    light.lookAt(testNodes[0]);
+    
+    light.setAmbientColor(ofFloatColor(1.0, 1.0, 1.0, 1.0));
+    light.setDiffuseColor(ofFloatColor(1.0, 1.0, 1.0));
+    light.setSpecularColor(ofFloatColor(1.0, 1.0, 1.0));
+    
+    
+    // *****    sounds  *****
     
     //FFTのサイズとバッファサイズを設定
     fft_size = 512;
@@ -42,7 +68,68 @@ void ofApp::setup(){
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
+    
+    ofEnableAlphaBlending();
+    
+    
+    // *****    SCENE 1  *****
+    
+    myFbo_00.begin();
+    
+    if (xFlag == false) {
+        xAxis = -10;
+        if (cam[0].getX() < -300) {
+            xFlag = true;
+        }
+    }else if (xFlag == true) {
+        xAxis = 10;
+        if (cam[0].getX() > 300) {
+            xFlag = false;
+        }
+    }
+    
+    if (yFlag == false) {
+        yAxis = -10;
+        if (cam[0].getY() < -300) {
+            yFlag = true;
+        }
+    }else if (yFlag == true) {
+        yAxis = 10;
+        if (cam[0].getY() > 300) {
+            yFlag = false;
+        }
+    }
+    
+    if (zFlag == false) {
+        zAxis = -10;
+        if (cam[0].getZ() < -500) {
+            zFlag = true;
+        }
+    }else if (zFlag == true) {
+        zAxis = 10;
+        if (cam[0].getZ() > 500) {
+            zFlag = false;
+        }
+    }
+    
+    cam[0].move(xAxis, yAxis, zAxis);
+    
+    drawBoxes();
+    myFbo_00.end();
+    
+    
+    // *****    SCENE 2  *****
+    
+    myFbo_01.begin();
+    
+    cam[1].move(-xAxis, -yAxis, -zAxis);
+    
+    drawWave();
+    myFbo_01.end();
+    
+    
+    // *****    sounds  *****
     
     //オーディオ入力をFFT解析 (左右2ch)
     avg_powerL = 0.0;
@@ -53,7 +140,133 @@ void ofApp::update(){
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::drawBoxes(){
+    
+    float movementSpeed = .1;
+    float cloudSize = ofGetWidth() / 2;
+    float maxBoxSize = 100;
+    float spacing = 1;
+    int boxCount = 100;
+    
+    ofEnableAlphaBlending();
+    ofClear(255, 255, 255, 0);
+    
+    for (int i=0; i<kNumCameras; i++) {
+        if (lookatIndex[i] >= 0) {
+            cam[i].lookAt(testNodes[lookatIndex[i]]);
+        }
+    }
+    
+    cam[0].begin();
+    
+    for(int i = 0; i < boxCount; i++) {
+        ofPushMatrix();
+        
+        float t = (ofGetElapsedTimef() + i * spacing) * movementSpeed;
+        ofVec3f pos(
+                    ofSignedNoise(t, 0, 0),
+                    ofSignedNoise(0, t, 0),
+                    ofSignedNoise(0, 0, t));
+        
+        float boxSize = maxBoxSize * ofNoise(pos.x, pos.y, pos.z);
+        
+        pos *= cloudSize;
+        ofTranslate(pos);
+        ofRotateX(pos.x);
+        ofRotateY(pos.y);
+        ofRotateZ(pos.z);
+        
+        //        ofNoFill();
+        //        ofSetColor(255);
+        //        ofSetLineWidth(5);
+        //        ofDrawBox(boxSize * 1.1f);
+        
+        ofFill();
+        ofSetColor(0, 200, 200);
+        //        ofSetColor(ofColor::fromHsb(sinf(t) * 128 + 128, 255, 255));
+        ofDrawBox(boxSize * 1.1f);
+        
+        ofPopMatrix();
+    }
+    
+    cam[0].end();
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::drawWave(){
+    
+    cam[1].begin();
+    
+    ofEnableAlphaBlending();
+    ofClear(255, 255, 255, 0);
+    ofSetColor(255);
+    s.set(100,16);
+    s.setPosition(300, 0, 40);
+    s.draw();
+    
+    cam[1].end();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+    
+    myGlitch_00.generateFx();
+    myGlitch_01.generateFx();
+    ofSetColor(255, 255, 255);
+    
+    if (switchScene == 0) {
+        myFbo_00.draw(0, 0);
+    }
+    else if(switchScene == 1){
+        myFbo_01.draw(0,0);
+    }
+    
+    // *****    sounds  *****
+    
+    /*
+     //FFT解析した結果をもとに、グラフを生成
+     float w = (float)ofGetWidth()/ (float)fft_size / 2.0f;
+     for (int i = 0; i < fft_size; i++) {
+     
+     //塗りのアルファ値でFFT解析結果を表現
+     ofColor col;
+     col.setHsb(i * 255.0f / (float)fft_size, 255, 255, 31);
+     ofSetColor(col);
+     
+     ofCircle(ofGetWidth()/2 - w * i, ofGetHeight()/2, magnitudeL[i] * ofGetWidth()/100.0); //左
+     ofCircle(ofGetWidth()/2 + w * i, ofGetHeight()/2, magnitudeR[i] * ofGetWidth()/100.0); //右
+     }
+     */
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+    switch (key) {
+        case 'e':
+            myGlitch_00.setFx(OFXPOSTGLITCH_SLITSCAN, true);
+            myGlitch_01.setFx(OFXPOSTGLITCH_TWIST, true);
+            break;
+        case '1':
+            switchScene = 0;
+            break;
+        case '2':
+            switchScene = 1;
+            break;
+        default:
+            break;
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+    if (key == 'e'){
+        myGlitch_00.setFx(OFXPOSTGLITCH_SLITSCAN, false);
+        myGlitch_01.setFx(OFXPOSTGLITCH_TWIST, false);
+    }
     
 }
 
@@ -65,65 +278,4 @@ void ofApp::audioIn(float* input, int bufferSize, int nChannels) {
         left[i]		= input[i*2];
         right[i]	= input[i*2+1];
     }
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-    
-    if (key == '1') {
-        stateMachine.changeState("circle");
-    }
-    if (key == '2') {
-        stateMachine.changeState("box");
-    }
-    if (key == '3') {
-        stateMachine.changeState("particle");
-    }
-    
-    // fキーでフルスクリーン表示
-    if(key == 'f'){
-        ofToggleFullscreen();
-    }
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-    
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,39 +1,54 @@
 #pragma once
 
 #include "ofMain.h"
-#include "ofxStateMachine.h"
-#include "SharedData.h"
+#include "ofxPostGlitch.h"
 #include "fft.h"
 
-class ofApp : public ofBaseApp{
-    
+#define kNumTestNodes 4
+#define kNumCameras 2
+
+class ofApp : public ofBaseApp {
 public:
     void setup();
     void update();
     void draw();
-    
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-    
-    itg::ofxStateMachine<SharedData> stateMachine;
-    
+    void keyPressed  (int key);
+    void keyReleased  (int key);
     void audioIn(float* input, int bufferSize, int nChannels);
     
+    void drawBoxes();
+    void drawWave();
+    
+    
+// *****    visual  *****
+    int camToView;
+    int lookatIndex[kNumCameras];
+    
+    float xAxis, yAxis, zAxis;
+    bool xFlag, yFlag, zFlag;
+    
+    ofLight light;
+    ofCamera cam[kNumCameras];
+    ofNode testNodes[kNumTestNodes];
+    
+    ofSpherePrimitive s;
+    
+    ofFbo			myFbo_00;
+    ofFbo			myFbo_01;
+    ofxPostGlitch	myGlitch_00;
+    ofxPostGlitch	myGlitch_01;
+    
+    int switchScene;
+    
+// *****    sounds  *****
     //左右2chのFFTの入出力の値を確保
     float *left, *right;
-    float *phaseL, *powerL;
-    float *phaseR, *powerR;
+    float *magnitudeL, *phaseL, *powerL;
+    float *magnitudeR, *phaseR, *powerR;
     float avg_powerL, avg_powerR;
-    
     //バッファーサイズとFFTサイズ
     int buffer_size;
+    int fft_size;
     
     fft myfft; //FFTクラスのインスタンス
     
